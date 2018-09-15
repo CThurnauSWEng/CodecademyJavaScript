93c888c877af5fc2f8c1136c0e38e819c13971ea
Carrie Thurnau <carriethurnau@Carries-MacBook-Pro.local>
2018-09-15 14:21:27 -0700

    updates to advanced Objects

--- advancedObjects.js
+++ advancedObjects.js
@@ -171,3 +171,16 @@ const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, rob
 console.log(newRobot);
 
 console.log(robot);
+
+let tempObj = {
+  _num: 22,
+  get num() {
+      return this._num;
+  }
+};
+
+//examples of ways to get num
+
+console.log(tempObj.num);
+console.log(tempObj[' num']);
+console.log(tempObj._num);
